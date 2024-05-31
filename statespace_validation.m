8803488f99c2524def7a4fa959786c2f70d1190d
Arifk259 <khana391@mcmaster.ca>
2024-05-31 10:34:31 -0400

    Update statespace_validation.m

--- statespace_validation.m
+++ statespace_validation.m
@@ -1,4 +1,8 @@
 %% DEFINE INITIAL CONDITIONS, INERTIA TENSOR
+clear;
+close;
+clc;
+
 
 % Body axes are initially aligned with inertial axes 
 q0 = 1;  
@@ -15,21 +19,21 @@ omega_z = 0;
 Ixx = 0.13614614;
 Iyy = 0.13608852;
 Izz = 0.00636670;
-Iyz = -0.00303665;
-Izy = -0.00303665;
+Iyz = 0; %-0.00303665;
+Izy = 0; %-0.00303665;
 
 % Define inertia tensor inverse
-axx = 7.3450484898066151563;
-ayy = 7.4272042201116417278;
-ayz = 3.5424662219049141394;
-azy = 3.5424662219049141394;
-azz = 158.75684892530629015;
+axx = 1/Ixx; %7.3450484898066151563;
+ayy = 1/Iyy; %7.4272042201116417278;
+ayz = 0; %3.5424662219049141394;
+azy = 0; %3.5424662219049141394;
+azz = 1/Izz; %158.75684892530629015;
 
 % Initial state vector
 x0 = [q0; q1; q2; q3; omega_x; omega_y; omega_z];
 
 % Control vector (no control)
-u = @(t) [0*t; 1; 0*t];
+u = @(t) [0; 0; 0];
 
 %% IN HOUSE STATE SPACE MODEL
 
@@ -39,7 +43,7 @@ A = @(x) [
  0.5*x(5)  0  0.5*x(7)  -0.5*x(6)  0.5*x(1)  -0.5*x(4)  0.5*x(3); 
  0.5*x(6)  -0.5*x(7)  0  0.5*x(5)  0.5*x(4)  0.5*x(1)  -0.5*x(2);
  0.5*x(7)  0.5*x(6)  -0.5*x(5)  0  -0.5*x(3)  0.5*x(2)  0.5*x(1);
- 0  0  0  0  0  axx*(Iyy*x(3) - 2*Izy*x(6) - Izz*x(7))  axx*(Iyy*x(6) + 2*Iyz*x(7) - Izz*x(6));
+ 0  0  0  0  0  axx*(Iyy*x(7) - 2*Izy*x(6) - Izz*x(7))  axx*(Iyy*x(6) + 2*Iyz*x(7) - Izz*x(6));
  0  0  0  0  ayy*(-Ixx*x(7) + Izy*x(6) + Izz*x(7)) + ayz*(Ixx*x(6) - Iyy*x(6) - Iyz*x(7))  ayy*Izy*x(5) + ayz*x(5)*(Ixx - Iyy)  ayy*x(5)*(Izz - Ixx) - ayz*Iyz*x(5);
  0  0  0  0  azy*(-Ixx*x(7) + Izy*x(6) + Izz*x(7)) + azz*(Ixx*x(6) - Iyy*x(6) - Iyz*x(7))  azy*Izy*x(5) + azz*x(5)*(Ixx - Iyy)  azy*x(5)*(Izz - Ixx) - azz*Iyz*x(5)
  ];
@@ -52,10 +56,17 @@ B = [0 0 0;
      0 ayy ayz;
      0 azy azz];
 
-stateSpaceModel = @(t,x) A(x)*x + B*u(t);
+%stateSpaceModel = @(t,x) A(x)*x + B*u(t);
+
+
+%Function to renormalize the quaternion
+normalize = @(x) [x(1:4) / norm(x(1:4)); x(5:7)];
+
+stateSpaceModel = @(t,x) A(normalize(x))*normalize(x) + B*u(t);
+
 
 % Set time span for simulation
-tspan = [0 20];
+tspan = [0 10];
 
 % Solve the state-space model using ode45
 [t, x] = ode45(stateSpaceModel, tspan, x0);
@@ -69,7 +80,19 @@ legend('x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7');
 title('State Evolution with State-Dependent Matrices');
 grid on;
 
-%% STATE SPACE MODEL FROM YAGUANG
+
+rows = size(x,1);
+for i=1:rows
+    Ex = (1/2)*Ixx*(x(i,5)^2);
+    Ey = (1/2)*Iyy*(x(i,6)^2);
+    Ez = (1/2)*Izz*(x(i,7)^2);
+    Etotal(i) = Ex + Ey + Ez;
+end
+
+transpose(Etotal)
+
+
+%% STATE SPACE MODEL FROM YAGUANG - Kyle
 
 A = @(x) 0.5*[
     0     -x(5)  -x(6)  -x(7)  0  0  0;
@@ -110,6 +133,49 @@ ylabel('States');
 legend('x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7');
 title('State Evolution with State-Dependent Matrices');
 grid on;
+
+%% STATE SPACE MODEL FROM YAGUANG - Arif
+
+A = @(x) 0.5*[
+    0  0  0  0  0  0  0;
+    0  0  0  0  1  0  0;
+    0  0  0  0  0  1  0;
+    0  0  0  0  0  0  1;
+    0  0  0  0  0  0  0;
+    0  0  0  0  0  0  0;
+    0  0  0  0  0  0  0;
+    ];
+
+B = [
+    0  0  0;
+    0  0  0;
+    0  0  0;
+    0  0  0;
+    axx  0  0;
+    0  ayy  ayz;
+    0  azy  azz
+    ];
+
+% Function to renormalize the quaternion
+normalize = @(x) [x(1:4) / norm(x(1:4)); x(5:7)];
+
+stateSpaceModel = @(t,x) A(normalize(x))*normalize(x) + B*u(t);
+
+% Set time span for simulation
+tspan = [0 20];
+
+% Solve the state-space model using ode45
+[t, x] = ode45(stateSpaceModel, tspan, x0);
+
+% Plot the results
+figure;
+plot(t, x);
+xlabel('Time (s)');
+ylabel('States');
+legend('x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7');
+title('State Evolution with State-Dependent Matrices');
+grid on;
+
 
 %%
 disp(x);
